99d97d3e947eda0298b5f63f65fb5ec73d63bb27
naveen51923 <32910071+naveen51923@users.noreply.github.com>
2019-02-25 14:46:59 +0530

    Create p4.java

---
+++ p4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class p4
+{
+	public static void main(String[ ] arg)
+	{
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+    int b=s.nextInt();
+	int c=s.nextInt();
+	if( a >= b && a >= c)
+	{
+            System.out.println(a);
+}
+        else if (b >= a && b >=c)
+            {System.out.println(b);
+}
+        else
+        {
+            System.out.println(c);
+        }}}
