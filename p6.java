e9ab9c67801369fee4ca905b472d1fc211910466
naveen51923 <32910071+naveen51923@users.noreply.github.com>
2019-02-25 14:58:00 +0530

    Create p6.java

---
+++ p6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class p6
+{
+	public static void main(String[ ] arg)
+	{
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+	if(a%4==0)
+	{
+	    System.out.print("yes");
+	}
+	else
+	{
+	    System.out.print("no");
+	}
+}
+}
