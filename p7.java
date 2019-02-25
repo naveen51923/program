308000e3654cb8e752df5127bc1ffeb1e49c5d01
naveen51923 <32910071+naveen51923@users.noreply.github.com>
2019-02-25 15:13:25 +0530

    Create p7.java

---
+++ p7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class p7
+{
+	public static void main(String[ ] arg)
+	{
+	Scanner s=new Scanner(System.in);
+	String a="Hello";
+	int n=s.nextInt();
+	for(int i=1;i<=n;i++)
+	{
+	    System.out.println(a);
+	}
+}
+}
